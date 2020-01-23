d8c959a965d8118f0cf76e9695f2997916d9d972
Jaap Frolich <jfrolich@gmail.com>
2020-01-23 14:05:53 +0800

    Fix

--- copyPlatformBinaryInPlace.js
+++ copyPlatformBinaryInPlace.js
@@ -1,34 +1,37 @@
 #!/usr/bin/env node
 
-var fs = require("fs");
+var fs = require('fs');
 
 var arch = process.arch;
 var platform = process.platform;
 
-if (arch === "ia32") {
-  arch = "x86";
+if (arch === 'ia32') {
+  arch = 'x86';
 }
 
-if (platform === "win32") {
-  platform = "win";
+if (platform === 'win32') {
+  platform = 'win';
 }
 
-copyBinary("bin/graphql_ppx-" + platform + "-" + arch + ".exe", "ppx");
-copyBinary("bin/bsb6/graphql_ppx-" + platform + "-" + arch + ".exe", "ppx6");
+copyBinary('bin/bsb6/graphql_ppx-' + platform + '-' + arch + '.exe', 'ppx6');
 
 function copyBinary(filename, destFilename) {
   var supported = fs.existsSync(filename);
 
   if (!supported) {
-    console.error("graphql_ppx does not support this platform :(");
-    console.error("");
-    console.error("graphql_ppx comes prepacked as built binaries to avoid large");
-    console.error("dependencies at build-time.");
-    console.error("");
-    console.error("If you want graphql_ppx to support this platform natively,");
-    console.error("please open an issue at our repository, linked above. Please");
-    console.error("specify that you are on the " + platform + " platform,");
-    console.error("on the " + arch + " architecture.");
+    console.error('graphql_ppx does not support this platform :(');
+    console.error('');
+    console.error(
+      'graphql_ppx comes prepacked as built binaries to avoid large',
+    );
+    console.error('dependencies at build-time.');
+    console.error('');
+    console.error('If you want graphql_ppx to support this platform natively,');
+    console.error(
+      'please open an issue at our repository, linked above. Please',
+    );
+    console.error('specify that you are on the ' + platform + ' platform,');
+    console.error('on the ' + arch + ' architecture.');
 
     if (!process.env.IS_GRAPHQL_PPX_CI) {
       process.exit(1);
@@ -37,7 +40,7 @@ function copyBinary(filename, destFilename) {
 
   if (process.env.IS_GRAPHQL_PPX_CI) {
     console.log(
-      "graphql_ppx: IS_GRAPHQL_PPX_CI has been set, skipping moving binary in place"
+      'graphql_ppx: IS_GRAPHQL_PPX_CI has been set, skipping moving binary in place',
     );
     process.exit(0);
   }
@@ -47,7 +50,7 @@ function copyBinary(filename, destFilename) {
     fs.chmodSync(destFilename, 0755);
   }
 
-  var destFilenameExe = destFilename + ".exe"
+  var destFilenameExe = destFilename + '.exe';
   if (!fs.existsSync(destFilenameExe)) {
     copyFileSync(filename, destFilenameExe);
     fs.chmodSync(destFilenameExe, 0755);
@@ -55,7 +58,7 @@ function copyBinary(filename, destFilename) {
 }
 
 function copyFileSync(source, dest) {
-  if (typeof fs.copyFileSync === "function") {
+  if (typeof fs.copyFileSync === 'function') {
     fs.copyFileSync(source, dest);
   } else {
     fs.writeFileSync(dest, fs.readFileSync(source));
